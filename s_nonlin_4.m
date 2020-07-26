e9d1076377fa54c2aac209fe184c185daeb5c951
mihailm94 <mihailmilchev@gmail.com>
2020-07-26 16:38:35 +0200

    Update files bearbeiter a

--- s_nonlin_4.m
+++ s_nonlin_4.m
@@ -1,17 +1,11 @@
 function sz = s_nonlin_4(t, z)
     
     % c should be defined as a global struct
-    %global c;
+    global c;
     
     global S_0;
-    
-    %sz(:,1) = 1:length(z);
-
-    %for i = 1:length(z)
-    %sz = S_0*exp(-( (t.*t)/(2*(0.01*0.01)) ))*exp( -((c.alpha).*z) );
-    
-    alpha = 10^5;
-    sz = S_0*exp(((-t.^2)/(2*0.01.^2))*exp(-alpha*z));
-    %end
+        
+    c.alpha = 10^5;
+    sz = S_0*exp(((-t.^2)/(2*0.01.^2))*exp(-c.alpha*z));
 
 end
